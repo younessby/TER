a35d0260054d2e9e491d4ddfc8da26eb43deff13
younesseirb <younesseirb@github.com>
2024-04-23 13:28:31 +0200

    version 23/04/2024

--- programme.f90
+++ programme.f90
@@ -14,7 +14,7 @@ program programme
     type(edge),allocatable,dimension(:)::lesaretes,aretes_delaunay
     type(triangle),allocatable,dimension(:)::lestriangles,triangles_tri_super,triangles_tri,tri_delaunay
     integer,allocatable, dimension(:,:):: aretes_maille,maille_aretes,voronoi
-    integer,allocatable,dimension(:,:)::maille_point
+    integer,allocatable,dimension(:,:)::maille_point,centre_bord
     integer,allocatable,dimension(:)::point_bord1,edge_bord1,point_bord,edge_bord,poin_tri,poin_tri1
     type(edge)::aretes_deb1,arretes_ari,aretes_deb
     type(point)::p1,p2,p3
@@ -82,96 +82,78 @@ program programme
         end do 
 
     call save_vtk(triangles_tri(1:nb_tri1),lespoints,"test.vtk",n+3,nb_tri1)
-    allocate(poin_tri(3*nb_point))
-    nb_point_aj=0
-        do i=1,nb_tri1
-            if(hasObtuseAngle(triangles_tri(i)%p1,triangles_tri(i)%p2,triangles_tri(i)%p3) .eqv. .false.) then
-                nb_point_aj=nb_point_aj+1
-                point_ajouter(nb_point_aj)=getMidpointOfObtuseSide(triangles_tri(i)%p1,triangles_tri(i)%p2,triangles_tri(i)%p3)
-                !print*,"errrrrrrrrr",triangles_tri(i)%p1%indice,triangles_tri(i)%p2%indice,triangles_tri(i)%p3%indice
-            end if 
-        end do 
-    call removeDuplicates(poin_tri(1:compteur_pointtri),poin_tri1,compteur_pointtri)
-    allocate(point_delaunay(nb_point+nb_point_aj))
+!     allocate(point_ajouter(nb_point))
+!     nb_point_aj=0
+!         do i=1,nb_tri1
+!             if(hasObtuseAngle(triangles_tri(i)%p1,triangles_tri(i)%p2,triangles_tri(i)%p3) .eqv. .true.) then
+!                 nb_point_aj=nb_point_aj+1
+!                 point_ajouter(nb_point_aj)=getMidpointOfObtuseSide(triangles_tri(i)%p1,triangles_tri(i)%p2,triangles_tri(i)%p3)
+!                 !print*,"errrrrrrrrr",triangles_tri(i)%p1%indice,triangles_tri(i)%p2%indice,triangles_tri(i)%p3%indice
+!             end if 
+!         end do 
+!     print*,"point ajouter ",nb_point_aj
+!    ! call removeDuplicates(poin_tri(1:compteur_pointtri),poin_tri1,compteur_pointtri)
+!     allocate(point_delaunay(nb_point+nb_point_aj))
 
-    do i=1,nb_point
-        !print*,"errrrrrr",poin_tri1(i),i,compteur_pointtri
-        point_delaunay(i)=lespoints(i)
-        point_delaunay(i)%indice=i
-    end do 
-    do i=1,nb_point_aj
-            point_delaunay(i+nb_point)=point_ajouter(i)
-            point_delaunay(i+nb_point)%indice=i+nb_point
-    end do
-    nb_point=nb_point+nb_point_aj
-    deallocate(poin_tri,poin_tri1,lespoints)
-    allocate(lespoints(nb_point))
-    lespoints=point_delaunay
+!     do i=1,nb_point
+!         !print*,"errrrrrr",poin_tri1(i),i,compteur_pointtri
+!         point_delaunay(i)=lespoints(i)
+!         point_delaunay(i)%indice=i
+!     end do 
+!     do i=1,nb_point_aj
+!             point_delaunay(i+nb_point)=point_ajouter(i)
+!             point_delaunay(i+nb_point)%indice=i+nb_point
+!     end do
+!     deallocate(lespoints)
+!     nb_point=nb_point+nb_point_aj
+!     allocate(lespoints(nb_point))
+!     lespoints=point_delaunay
 
-    deallocate(point_delaunay,triangles_tri,lestriangles,triangles_tri_super)
+!     deallocate(point_delaunay,triangles_tri,lestriangles,triangles_tri_super,point_ajouter)
 
-!%%%%%%%%%%%%%%%%%%%%%%%hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-    super = SuperTriangle(lespoints)
-    call construction_points(lespoints(1),super%p1%x,super%p1%y,1)
-    call construction_points(lespoints(2),super%p2%x,super%p2%y,2)
-    call construction_points(lespoints(3),super%p3%x,super%p3%y,3)
+! !%%%%%%%%%%%%%%%%%%%%%%%hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+!     super = SuperTriangle(lespoints)
+!     call construction_points(lespoints(1),super%p1%x,super%p1%y,1)
+!     call construction_points(lespoints(2),super%p2%x,super%p2%y,2)
+!     call construction_points(lespoints(3),super%p3%x,super%p3%y,3)
     
-    allocate(lestriangles(3*nb_point))
+!     allocate(lestriangles(3*nb_point))
 
-    do i=1,3*nb_point
-        lestriangles(i)=super
-    end do
+!     do i=1,3*nb_point
+!         lestriangles(i)=super
+!     end do
 
-    call algo_tri(lespoints,lestriangles,nb_point)
+!     call algo_tri(lespoints,lestriangles,nb_point)
 
-    !trier les triangles 
-    allocate(triangles_tri_super(3*nb_point))
-    nb_tri=1
-    triangles_tri_super(1)=super
-    do i=1,3*nb_point
-       if(lestriangles(i)%p1%indice/=1 .or. lestriangles(i)%p2%indice/=2 &
-       .or. lestriangles(i)%p3%indice/=3)then
-        triangles_tri_super(i+1)=lestriangles(i)
-            nb_tri=nb_tri+1
-       end if 
-    end do 
-    !call save_vtk(triangles_tri_super(1:nb_tri),lespoints,"testt.vtk",n+3,nb_tri)
+!     !trier les triangles 
+!     allocate(triangles_tri_super(3*nb_point))
+!     nb_tri=1
+!     triangles_tri_super(1)=super
+!     do i=1,3*nb_point
+!        if(lestriangles(i)%p1%indice/=1 .or. lestriangles(i)%p2%indice/=2 &
+!        .or. lestriangles(i)%p3%indice/=3)then
+!         triangles_tri_super(i+1)=lestriangles(i)
+!             nb_tri=nb_tri+1
+!        end if 
+!     end do 
+!     !call save_vtk(triangles_tri_super(1:nb_tri),lespoints,"testt.vtk",n+3,nb_tri)
 
-    allocate(triangles_tri(3*nb_tri))
-        nb_tri1=0
-        do i=1,nb_tri
-            if(triangles_tri_super(i)%p1%indice/=1 .and. triangles_tri_super(i)%p1%indice/=2 &
-            .and. triangles_tri_super(i)%p1%indice/=3 .and. triangles_tri_super(i)%p2%indice/=1 &
-            .and. triangles_tri_super(i)%p2%indice/=2 .and. triangles_tri_super(i)%p2%indice/=3 &
-            .and. triangles_tri_super(i)%p3%indice/=1 .and. triangles_tri_super(i)%p3%indice/=2 &
-            .and. triangles_tri_super(i)%p3%indice/=3)then
-                nb_tri1=nb_tri1+1
-                triangles_tri(nb_tri1)=triangles_tri_super(i)
-                ! print*,triangles_tri(nb_tri1)%p1%indice,triangles_tri(nb_tri1)%p2%indice,&
-                ! triangles_tri(nb_tri1)%p3%indice,nb_tri1
-            end if
-        end do 
+!     allocate(triangles_tri(3*nb_tri))
+!         nb_tri1=0
+!         do i=1,nb_tri
+!             if(triangles_tri_super(i)%p1%indice/=1 .and. triangles_tri_super(i)%p1%indice/=2 &
+!             .and. triangles_tri_super(i)%p1%indice/=3 .and. triangles_tri_super(i)%p2%indice/=1 &
+!             .and. triangles_tri_super(i)%p2%indice/=2 .and. triangles_tri_super(i)%p2%indice/=3 &
+!             .and. triangles_tri_super(i)%p3%indice/=1 .and. triangles_tri_super(i)%p3%indice/=2 &
+!             .and. triangles_tri_super(i)%p3%indice/=3)then
+!                 nb_tri1=nb_tri1+1
+!                 triangles_tri(nb_tri1)=triangles_tri_super(i)
+!                 ! print*,triangles_tri(nb_tri1)%p1%indice,triangles_tri(nb_tri1)%p2%indice,&
+!                 ! triangles_tri(nb_tri1)%p3%indice,nb_tri1
+!             end if
+!         end do 
 
-    call save_vtk(triangles_tri(1:nb_tri1),lespoints,"tett.vtk",nb_point,nb_tri1)
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+!     call save_vtk(triangles_tri(1:nb_tri1),lespoints,"tett.vtk",nb_point,nb_tri1)
 
 
 
@@ -185,10 +167,6 @@ deallocate(lestriangles)
 deallocate(triangles_tri,triangles_tri_super)
 
 !!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Voronoi %%%%%%%%%
-    allocate(lescentres(1:nb_tri1))
-    do i=1,nb_tri1
-        call construction_points_center(tri_delaunay(i),lescentres(i))
-    end do 
 
 
     nb_edg0=3*nb_tri1
@@ -209,7 +187,7 @@ deallocate(triangles_tri,triangles_tri_super)
     close(10)
 
     allocate(aretes_delaunay(nb_aretes),edge_bord1(nb_aretes),point_bord1(nb_aretes))
-        !trouver les arrtes et le spoints au bord
+        !trouver les arrtes et les points au bord
     compteur_edge = 0
     compteur_point=0
     do i=1,nb_aretes
@@ -231,6 +209,24 @@ deallocate(triangles_tri,triangles_tri_super)
 
     print *,"points au bord",size(point_bord)
     print *,"edge au bord",compteur_edge
+    
+    allocate(centre_bord(size(edge_bord),4))
+    do i=1,size(edge_bord)
+        centre_bord(i,1)=edge_bord(i)
+        centre_bord(i,2)=lesaretes(edge_bord(i))%p1%indice
+        centre_bord(i,3)=lesaretes(edge_bord(i))%p2%indice
+    end do
+
+    allocate(lescentres(1:nb_tri1+size(point_bord)))
+    do i=1,nb_tri1
+        call construction_points_center(tri_delaunay(i),lescentres(i))
+    end do 
+    do i=nb_tri1+1,nb_tri1+size(edge_bord)
+        lescentres(i)=findMidpoint(lespoints(centre_bord(i-nb_tri1,2)),lespoints(centre_bord(i-nb_tri1,3)))
+        lescentres(i)%indice=i
+        centre_bord(i-nb_tri1,4)=i
+    end do 
+
 
     do i=1,nb_aretes
         aretes_delaunay(i)=lesaretes(i)
@@ -238,7 +234,7 @@ deallocate(triangles_tri,triangles_tri_super)
     deallocate(lesaretes)
     print *, "je suis ici"
     
-
+    voronoi=0
     do i=4,nb_point
         bord=.false.
         do j=1,size(point_bord)
@@ -247,6 +243,7 @@ deallocate(triangles_tri,triangles_tri_super)
             end if 
         end do 
         if(bord .eqv. .false. ) then 
+            voronoi(i,1)=maille_point(i,1)
             !print*,"point ",i
             allocate(voronoi_entre(maille_point(i,1)),voronoi_sort(maille_point(i,1)))
             do j=1,maille_point(i,1)
@@ -259,9 +256,39 @@ deallocate(triangles_tri,triangles_tri_super)
             write(*,'(i3)', advance='no')maille_point(i,1)
             do j=1,size(voronoi_entre)
                 write(*,'(i3)', advance='no')voronoi_sort(j)%indice-1
+                voronoi(i,j+1)=voronoi_sort(j)%indice-1
             end do 
             write(*,*) "" 
             deallocate(voronoi_entre,voronoi_sort)
+        end if 
+        if(bord .eqv. .true.) then
+            voronoi(i,1)=maille_point(i,1)+2
+            allocate(voronoi_entre(maille_point(i,1)+2),voronoi_sort(maille_point(i,1)+2))
+            do j=1,maille_point(i,1)
+                voronoi_entre(j)=lescentres(maille_point(i,j+1))
+                !write(*,'(i3)', advance='no')voronoi_entre(j)%indice
+            end do
+            compteur_point=0
+            do j=1,size(edge_bord)
+                if(centre_bord(j,2)==i .or. centre_bord(j,3)==i  ) then 
+                    compteur_point=compteur_point+1
+                    voronoi_entre(maille_point(i,1)+compteur_point)=lescentres(centre_bord(j,4))
+                end if
+                if(compteur_point>2) then 
+                    print*,"j'ai un probleme au bord ligne 275"
+                end if 
+            end do 
+            write(*,*) "" 
+            call sortPointsCCW(lespoints(i),voronoi_entre,voronoi_sort)
+            !print*,"point ",i
+            write(*,'(i3)', advance='no')maille_point(i,1)
+            do j=1,size(voronoi_entre)
+                write(*,'(i3)', advance='no')voronoi_sort(j)%indice-1
+                voronoi(i,j+1)=voronoi_sort(j)%indice-1
+            end do 
+            write(*,*) "" 
+            deallocate(voronoi_entre,voronoi_sort)
+
         end if 
 
 
@@ -366,11 +393,11 @@ deallocate(triangles_tri,triangles_tri_super)
 !                     end if 
 !                 end do 
 !    end do 
-!    nb_cel=0
-!    do i=4,nb_point
-!     nb_cel=nb_cel+voronoi(i,1)
-!    end do 
-!    !nb_cel=nb_cel+nb_point-3
+   nb_cel=0
+   do i=4,nb_point
+   nb_cel=nb_cel+voronoi(i,1)
+   end do 
+   nb_cel=nb_cel+nb_point-3
 
    open(unit=10, file="vorn.vtk", status='replace')
    ! Write informations
@@ -379,14 +406,14 @@ deallocate(triangles_tri,triangles_tri_super)
    write(10, '(A)') "ASCII"
    write(10, '(A)') "DATASET POLYDATA"
    ! Write vertex coordinates
-   write(10, '(A,I0,A)') "POINTS ", nb_tri1, " float"
-   do i = 1, nb_tri1
+   write(10, '(A,I0,A)') "POINTS ", nb_tri1+size(edge_bord), " float"
+   do i = 1, nb_tri1+size(edge_bord)
        write(10, '(2(F7.4,1X),F4.1)') lescentres(i)%x , lescentres(i)%y , 0.00000000
    end do
    ! Write cell connectivity
    write(10, '(A,I0,A,I0)') "POLYGONS ", nb_point-3, " ", nb_cel
    do i = 4,nb_point
-        do j=1,10
+        do j=1,voronoi(i,1)+1
             write(10, '(4(I0,1X))', ADVANCE='NO') voronoi(i,j)
         end do 
         write(10,*)
